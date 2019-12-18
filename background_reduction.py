8b81f2852df77e53e7587ed3d38cb1a456808870
LevanSulimanov <levansulimanov1996@gmail.com>
2019-12-18 17:14:17 -0500

    background_reduction - working on HSV colors

---
+++ background_reduction.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np 
+
+# set to 0, if you use webcam
+# otherwise, needs to be replaced with video, update is in development
+cap = cv2.VideoCapture(0)
+
+while True:
+	_, frame = cap.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	lower_red = np.array([50, 0, 0])
+	upper_red = np.array([255, 255, 255])
+
+	mask = cv2.inRange(hsv, lower_red, upper_red)
+	res = cv2.bitwise_and(frame, frame, mask = mask)
+
+	cv2.imshow('frame', frame)
+	cv2.imshow('mask', mask)
+	cv2.imshow('res', res)
+
+	k = cv2.waitKey(5) & 0xFF
+	if k == 27:
+		break
+
+cv2.destroyAllWindows()
+cap.release()
