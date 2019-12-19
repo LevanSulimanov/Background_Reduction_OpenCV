1440d193911ad3592ef7fd58b1223fe2a06a0190
LevanSulimanov <levansulimanov1996@gmail.com>
2019-12-18 22:33:19 -0500

    Update background_reduction.py

--- background_reduction.py
+++ background_reduction.py
@@ -3,25 +3,34 @@ import numpy as np
 
 # set to 0, if you use webcam
 # otherwise, needs to be replaced with video, update is in development
-cap = cv2.VideoCapture(0)
 
-while True:
-	_, frame = cap.read()
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def background_reduction():
 
-	lower_red = np.array([0, 0, 0])
-	upper_red = np.array([30, 255, 255])
+	cap = cv2.VideoCapture(0)
+	fgbg = cv2.createBackgroundSubtractorMOG2()
 
-	mask = cv2.inRange(hsv, lower_red, upper_red)
-	res = cv2.bitwise_and(frame, frame, mask = mask)
+	while True:
+		ret, frame = cap.read()
+		fgmask = fgbg.apply(frame)
+		#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	cv2.imshow('frame', frame)
-	cv2.imshow('mask', mask)
-	cv2.imshow('res', res)
+		#lower_red = np.array([0, 0, 0])
+		#upper_red = np.array([150, 150, 150])
 
-	k = cv2.waitKey(5) & 0xFF
-	if k == 27:
-		break
+		#mask = cv2.inRange(hsv, lower_red, upper_red)
+		#res = cv2.bitwise_and(frame, frame, mask = mask)
 
-cv2.destroyAllWindows()
-cap.release()
+		cv2.imshow('original', frame)
+		#cv2.imshow('frame', frame)
+		#cv2.imshow('mask', mask)
+		#cv2.imshow('res', res)
+		cv2.imshow('fg', fgmask)
+
+		k = cv2.waitKey(5) & 0xFF
+		if k == 27:
+			break
+
+	cv2.destroyAllWindows()
+	cap.release()
+
+background_reduction()
