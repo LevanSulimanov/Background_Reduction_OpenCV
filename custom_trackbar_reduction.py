9de5a6ed25db93252e1b5e987445fc9ceff96e95
LevanSulimanov <levansulimanov1996@gmail.com>
2019-12-24 17:23:06 -0500

    Update custom_trackbar_reduction.py

--- custom_trackbar_reduction.py
+++ custom_trackbar_reduction.py
@@ -15,9 +15,9 @@ cv2.createTrackbar("Lower H", "HSV Trackbars", 0, 179, nothing)
 cv2.createTrackbar("Lower S", "HSV Trackbars", 0, 255, nothing)
 cv2.createTrackbar("Lower V", "HSV Trackbars", 0, 255, nothing)
 
-cv2.createTrackbar("Upper H", "HSV Trackbars", 0, 179, nothing)
-cv2.createTrackbar("Upper S", "HSV Trackbars", 0, 255, nothing)
-cv2.createTrackbar("Upper V", "HSV Trackbars", 0, 255, nothing)
+cv2.createTrackbar("Upper H", "HSV Trackbars", 179, 179, nothing)
+cv2.createTrackbar("Upper S", "HSV Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Upper V", "HSV Trackbars", 255, 255, nothing)
 
 while True:
 	_, frame = cap.read()
@@ -33,12 +33,15 @@ while True:
 	u_v = cv2.getTrackbarPos("Upper V", "HSV Trackbars")
 
 
-	lower_col = np.array([0, 0, 0])
-	upper_col = np.array([180, 255, 255])
+	lower_col = np.array([l_h, l_s, l_v])
+	upper_col = np.array([u_h, u_s, u_v])
 	mask = cv2.inRange(hsv, lower_col, upper_col)
+
+	result = cv2.bitwise_and(frame, frame, mask=mask)
 
 	cv2.imshow("frame", frame)
 	cv2.imshow("mask", mask)
+	cv2.imshow("result", result)
 
 	key = cv2.waitKey(1)
 
